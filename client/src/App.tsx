2839417811d0ae1b9656507d11b142bff373b89e
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-08-27 15:14:52 +0900

    Feat: Add route for / and display the Home template

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/templates/Home";
-
 
 function App() {
   return (
-    <Home />
+    <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
