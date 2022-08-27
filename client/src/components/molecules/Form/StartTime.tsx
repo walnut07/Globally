ee4368c91e10d0695db360bdb16090dbe23fd8be
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-08-27 16:14:34 +0900

    Feat: Apply React Bootstrap to the StartTime and EndTime components

--- client/src/components/molecules/Form/StartTime.tsx
+++ client/src/components/molecules/Form/StartTime.tsx
@@ -1,4 +1,6 @@
-import "./Form.css";
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 type Props = {
 
@@ -8,8 +10,14 @@ const StartTimeMolecule: React.FC<Props>  = ({}) => {
 
   return (
     <div className="start-time-molecule">
-      <label className={"startTime-molecule"} htmlFor="start time">Start time</label>
-        <input type="time" name="start time" />
+      <InputGroup size="sm" className="mb-3">
+        <InputGroup.Text id="inputGroup-sizing-sm">Start Time</InputGroup.Text>
+        <Form.Control
+          aria-label="Start Time"
+          aria-describedby="inputGroup-sizing-sm"
+          type="time"
+        />
+     </InputGroup>
     </div>
   );
 };
