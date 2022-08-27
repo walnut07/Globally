1a9022c18be15bbda6d76f7e0a0f6b2e09b8fc8a
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-08-27 11:48:13 +0900

    Refactor: Add city selector to AreaMolecule component

--- client/src/components/molecules/Form/Area.tsx
+++ client/src/components/molecules/Form/Area.tsx
@@ -3,11 +3,15 @@ import { useEffect, useState } from "react";
 import "./Form.css";
 
 type Props = {
-  setSelectedCountry: Function,
+
 }
 
-const AreaMolecule: React.FC<Props>  = ({setSelectedCountry}) => {
+const AreaMolecule: React.FC<Props>  = ({}) => {
+  const [cities, setCities] = useState<string[]>([]);
   const [countries, setCountries] = useState<string[]>([]);
+  const [selectedCountry, setSelectedCountry] = useState<string>("");
+  const [cityForm, setCityForm] = useState<any>();
+
   const BASE_URL = process.env.REACT_APP_PUBLIC_URL || "http://localhost:8000";
 
   // get country data on initial load
@@ -23,21 +27,48 @@ const AreaMolecule: React.FC<Props>  = ({setSelectedCountry}) => {
     });
   }, []);
 
+  // trigger for getting city data
   const getCity = (e: React.ChangeEvent<HTMLSelectElement>) =>  {
     const country = e.target.value;
     setSelectedCountry(country);
   }
 
-  return (
-    <label className={"area-molecule"} htmlFor="area">
+  // fetch city data once a country is selected
+  useEffect(() => {
+    axios.get(`${BASE_URL}/city/?country=${selectedCountry}`)
+    .then((res: any) => {
+      const unsortedCities = res.data;
+      const sortedCities = unsortedCities.sort();
+      setCities(sortedCities);
+    })
+    .catch((err: any) => {
+      console.log(err);
+    });
+  }, [selectedCountry]);
 
-      <select name="area" onChange={getCity} className={"area-atom"}> 
+  // make cities appear once fetching city data
+  useEffect(() => {
+    const select = 
+      <select name="city" className="city-select-atom"> 
+        {cities.map(city => {
+          return <option value={city}>{city}</option>
+        })}
+    </select>
+    setCityForm(select);
+  }, [cities])
+
+  return (
+    <div className="area-molecule">
+      <label className={"country-label-atom"} htmlFor="area">Country</label>
+      <select name="area" onChange={getCity} className={"area-select-atom"}> 
           {countries.map(country => {
             return <option value={country}>{country}</option>
           })}
       </select>
 
-    </label>
+      <label className={"city-label-atom"} htmlFor="city">City</label>
+      {cityForm}
+    </div>
   );
 };
 
