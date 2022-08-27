0242f4e4c4a4b32445fb6c1832c7c0143fe2f1a5
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-08-27 12:07:52 +0900

    Fix: Delete nonused variables

--- client/src/components/organisms/FormMeetingDate.tsx
+++ client/src/components/organisms/FormMeetingDate.tsx
@@ -1,49 +1,14 @@
-// import "./Header.css";
-import { useState, useEffect, HtmlHTMLAttributes } from "react";
 import AreaMolecule from "../molecules/Form/Area";
 import DateMolecule from "../molecules/Form/Date";
 import StartTimeMolecule from "../molecules/Form/StartTime";
 import EndTimeMolecule from "../molecules/Form/EndTime";
-
 import "../Style.css";
-const axios = require('axios')
-const BASE_URL = process.env.REACT_APP_PUBLIC_URL || "http://localhost:8000";
 
 type Props = {
-  className?: string,
-  title: string,
-  inputType: string,
-  isOption: boolean
+
 }
 
-const FormMeetingDateOrganism: React.FC<Props> = ({title, inputType, isOption}) => {
-  const [cities, setCities] = useState<string[]>([]);
-  const [selectedCountry, setSelectedCountry] = useState<string>("");
-  const [cityForm, setCityForm] = useState<any>();
-
-  useEffect(() => {
-    axios.get(`${BASE_URL}/city/?country=${selectedCountry}`)
-    .then((res: any) => {
-      const unsortedCities = res.data;
-      const sortedCities = unsortedCities.sort();
-      setCities(sortedCities);
-    })
-    .catch((err: any) => {
-      console.log(err);
-    });
-  }, [selectedCountry]);
-
-  // make cities appear once fetching city data
-  useEffect(() => {
-    const select = 
-      <select name={title} className={title}> 
-        {cities.map(city => {
-          return <option value={city}>{city}</option>
-        })}
-    </select>
-    setCityForm(select);
-  }, [cities])
-
+const FormMeetingDateOrganism: React.FC<Props> = ({}) => {
   
   return (
     <div className={"form-meeting-date"}>
