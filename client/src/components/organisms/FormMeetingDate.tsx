377ce3c57f1955d914fe3e9f67e9534702b3b9f2
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-08-27 15:25:57 +0900

    Feat: Add the FormTitle molecule and call it from form organisms

--- client/src/components/organisms/FormMeetingDate.tsx
+++ client/src/components/organisms/FormMeetingDate.tsx
@@ -2,6 +2,7 @@ import AreaMolecule from "../molecules/Form/Area";
 import DateMolecule from "../molecules/Form/Date";
 import StartTimeMolecule from "../molecules/Form/StartTime";
 import EndTimeMolecule from "../molecules/Form/EndTime";
+import FormTitle from "../molecules/Form/FormTitle";
 
 type Props = {
 
@@ -11,6 +12,7 @@ const FormMeetingDateOrganism: React.FC<Props> = ({}) => {
   
   return (
     <form className={"form-meeting-date"}>
+      <FormTitle text={"When and where is the meeting held?"} />
       <AreaMolecule />
       <DateMolecule />
       <StartTimeMolecule />
