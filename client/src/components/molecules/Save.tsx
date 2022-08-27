a9dd300ba6c69372d444073467926a219aa1cc3e
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-08-27 13:55:46 +0900

    Refactor: Create Save molecule and call it from Save Organism

---
+++ client/src/components/molecules/Save.tsx
@@ -0,0 +1,11 @@
+import "../Style.css";
+
+const Save = () => {
+  return (
+    <div className="save-molecule">
+      <button className="save-button-atom">Save</button>
+    </div>
+  );
+};
+
+export default Save;
---
+++ client/src/components/organisms/Save.tsx
@@ -0,0 +1,10 @@
+import SaveMolecule from "../molecules/Save";
+import "../Style.css";
+
+const Save = () => {
+  return (
+    <SaveMolecule />
+  );
+};
+
+export default Save;
